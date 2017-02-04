77f52b00133018862c403eaea287ef1eb8c04fd2
Marcel Rusu <marcelrusu0@gmail.com>
2017-02-04 15:03:26 -0500

    fixed syntax

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const request = require('request');
 app.use(bodyParser.json());
 
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
